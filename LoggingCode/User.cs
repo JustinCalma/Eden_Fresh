811d78d494438d78dc4612b101d39502d8ef66e7
kchu457 <60150323+kchu457@users.noreply.github.com>
2021-12-02 17:10:22 -0800

    Add files via upload

---
+++ LoggingCode/User.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Data.SqlClient;
+
+
+namespace EdenFresh
+{
+    class User
+    {
+        public int userId { get; set; }
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public string email { get; set; }
+        public string password { get; set; }
+        public string username { get; set; }
+
+        
+        public User(string firstName, string lastName, string email, string password, string username)
+        {
+            Random rnd = new Random();
+            int value = rnd.Next(100000, 999999);
+            this.firstName = firstName;
+            this.lastName = lastName;
+            this.email = email;
+            this.password = password;
+            this.username = username;
+            this.userId = value;
+        }
+    
+    }
+}
---
+++ LoggingCode/UserDemo.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace EdenFresh
+{
+    class UserDemo
+    {
+        static void Main(String[]args)
+        { 
+            UserRepo repo = new UserRepo(); 
+            User testUser = new User("Kayla","Chu", "email", "pass", "username");
+            LogWriter writer = new LogWriter();   
+        }
+
+
+    }
+}
