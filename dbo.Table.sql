f120c5eed7be5e6cafe8c1812c4467f16105a89d
kchu457 <60150323+kchu457@users.noreply.github.com>
2021-12-02 16:48:14 -0800

    Delete dbo.Table.sql

--- dbo.Table.sql
+++
@@ -1,9 +0,0 @@
-﻿CREATE TABLE [dbo].[USERS]
-(
-	[UserId] INT NOT NULL PRIMARY KEY,
-	[FirstName] VARCHAR(50) NOT NULL,
-	[LastName] VARCHAR(50) NOT NULL,
-	[UserName] VARCHAR(50) NOT NULL,
-	[Password] VARCHAR(50) NOT NULL,
-	[Email] VARCHAR(50) NOT NULL
-)
