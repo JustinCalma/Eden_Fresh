b998afd77153746552eb4a2d5fb4d5133d814bf7
kchu457 <60150323+kchu457@users.noreply.github.com>
2021-12-02 17:43:51 -0800

    User and Logger class

---
+++ Application Code/EdenFreshApplication/LogWriter.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace EdenFresh
+{
+    class LogWriter
+    {
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=logging;Integrated Security=True;Pooling=False";
+        private int logId;
+        private int eventTypeId;
+        private int userId;
+        private DateTime timeStamp;
+        private string catagory;
+        private string description;
+        private Random rnd;
+        public LogWriter()
+        {
+
+        }
+        public LogWriter(int userId, int evenTypeId, DateTime timeStamp, string category, string description)
+        {
+            rnd = new Random();
+            int value = rnd.Next(100000, 999999);
+            this.logId = value;
+            this.userId = userId;
+            this.eventTypeId = evenTypeId;
+            this.timeStamp = timeStamp;
+            this.catagory = category;
+            this.description = description;
+        }
+
+        //CRUD: CREATE READ UPDATE DELETE
+        //Create message
+        public void write()
+        {
+            rnd = new Random();
+            int value = rnd.Next(100000, 999999);
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "INSERT INTO LOGGER (LogId, UserId, EventTypeId, EventTime, Category, Description) VALUES (@logId, @userId, @eventTypeId, @eventTime, @category, @description)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@logId", logId);
+                cmd.Parameters.AddWithValue("@userId", userId);
+                cmd.Parameters.AddWithValue("@eventTypeId", eventTypeId);
+                cmd.Parameters.AddWithValue("@eventTime", timeStamp);
+                cmd.Parameters.AddWithValue("@category", catagory);
+                cmd.Parameters.AddWithValue("@description", description);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Log Records Inserted Successfully");
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627)//SQL Duplicate primary key exception 
+                    {
+                        Console.WriteLine("Duplicate Key...reassigning key");
+
+                        logId = value;
+                        write();
+                    }
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+
+        //Read logged action/error of a single user
+        public void readUserId(int userId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "Select * from Logger where UserId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", userId);
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString());
+                    }
+                    Console.WriteLine("Logger Records read Successfully");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+
+        //read all logged event type 
+        public void readEventTypeId(int eventId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "Select * from Logger where eventTypeId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", eventId);
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString() + " " + reader[5].ToString());
+                    }
+                    Console.WriteLine("Logger Records read Successfully");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+
+        public void deleteUserLogEntry(int userId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                string query = "DELETE FROM logger WHERE UserId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", userId);
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("User Log Records Deleted Successfully");
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void deleteLogEntry(int logId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                string query = "DELETE FROM logger WHERE logId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", logId);
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Log Records Deleted Successfully");
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        //UPDATE LOG DOES NOT WORK 
+        public void updateLog()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "UPDATE Users SET LogId = @logId, UserId = @userId, EventTypeId = @eventTypeId, EventTime = @eventTime, Category = @category, Description = @description";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@logId", logId);
+                cmd.Parameters.AddWithValue("@userId", userId);
+                cmd.Parameters.AddWithValue("@eventTypeId", eventTypeId);
+                cmd.Parameters.AddWithValue("@eventTime", timeStamp);
+                cmd.Parameters.AddWithValue("@category", catagory);
+                cmd.Parameters.AddWithValue("@description", description);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Log Records Updated Successfully");
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine(ex.Message);
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+
+            }
+        }
+
+    }
+}
---
+++ Application Code/EdenFreshApplication/UserRepo.cs
@@ -0,0 +1,229 @@
+﻿using System;
+using System.Data.SqlClient;
+
+namespace EdenFresh
+{
+    // data access layer
+    class UserRepo : IUserDAO
+    {
+        private string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=logging;Integrated Security=True;Pooling=False";
+        private LogWriter logWriter;
+        public void createUser(User user)
+        {
+
+            Random rnd = new Random();
+            int value = rnd.Next(100000, 999999); // 6 digit generator
+
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                string query = "INSERT INTO Users (UserId, FirstName, LastName, Username, Password, Email) VALUES (@id, @firstN, @lastN, @userN, @pass, @email)";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", user.userId);
+                cmd.Parameters.AddWithValue("@firstN", user.firstName);
+                cmd.Parameters.AddWithValue("@lastN", user.lastName);
+                cmd.Parameters.AddWithValue("@userN", user.username);
+                cmd.Parameters.AddWithValue("@pass", user.password);
+                cmd.Parameters.AddWithValue("@email", user.email);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Records Inserted Successfully: User");
+                    logWriter = new LogWriter(user.userId, 3, DateTime.Now, "CREATE", "INSERT NEW USER INTO RECORDS SUCCESSFULLY");
+                    logWriter.write();
+                }
+                catch (SqlException ex)
+                {
+                    if (ex.Number == 2627)
+                    {
+                        Console.WriteLine("Duplicate Key...reassigning key");
+                        user.userId = value;
+                        logWriter = new LogWriter(user.userId, 6, DateTime.Now, "SQLEXCEPTION", "Reassigning userId key");
+                        logWriter.write();
+                        createUser(user);
+                    }
+                    else
+                    {
+                        Console.WriteLine(ex.ToString());
+                        logWriter = new LogWriter(user.userId, 0, DateTime.Now, "SQLEXCEPTION", ex.Message);
+                        logWriter.write();
+                    }
+
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                logWriter = new LogWriter(user.userId, 1, DateTime.Now, "SQLEXCEPTION", ex.Message);
+                logWriter.write();
+            }
+        }
+
+
+        public void deleteUser(int userId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+
+                string query = "DELETE FROM Users WHERE UserId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", userId);
+
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Records Deleted Successfully: User");
+                    logWriter = new LogWriter(userId, 2, DateTime.Now, "DELETE", "DELETE USER FROM RECORDS SUCCESSFULLY");
+                    logWriter.write();
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                    logWriter = new LogWriter(userId, 0, DateTime.Now, "SQLEXCEPTION", ex.Message);
+                    logWriter.write();
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                logWriter = new LogWriter(userId, 1, DateTime.Now, "EXCEPTION", ex.Message);
+                logWriter.write();
+            }
+        }
+
+        public void getAllUsers()
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "Select * from Users ";
+                SqlCommand cmd = new SqlCommand(query, con);
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString());
+                    }
+                    Console.WriteLine("All Records read Successfully");
+                }
+                catch (SqlException e)
+                {
+                    Console.WriteLine("Error: " + e.ToString());
+
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+
+        public void readUser(int userId)
+        {
+            try
+            {
+                SqlConnection con = new SqlConnection(connectionString);
+                string query = "Select * from Users where UserId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", userId);
+                try
+                {
+                    con.Open();
+                    SqlDataReader reader = cmd.ExecuteReader();
+                    while (reader.Read())
+                    {
+                        Console.WriteLine(reader[0].ToString() + " " + reader[1].ToString() + " " + reader[2].ToString() + " " + reader[3].ToString() + " " + reader[4].ToString());
+                    }
+                    Console.WriteLine("Records read Successfully");
+                    logWriter = new LogWriter(userId, 4, DateTime.Now, "READ", "READ FROM RECORDS SUCCESSFULLY");
+                    logWriter.write();
+
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                    logWriter = new LogWriter(userId, 0, DateTime.Now, "SQLEXCEPTION", ex.Message);
+                    logWriter.write();
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                logWriter = new LogWriter(userId, 1, DateTime.Now, "EXCEPTION", ex.Message);
+                logWriter.write();
+            }
+        }
+
+        public void updateUser(User user)
+        {
+            try
+            {
+
+                SqlConnection con = new SqlConnection(connectionString);
+
+                string query = "UPDATE Users SET FirstName = @firstN, LastName = @lastN, Username = @userN, password = @pass, email = @email WHERE UserId = @id";
+                SqlCommand cmd = new SqlCommand(query, con);
+                cmd.Parameters.AddWithValue("@id", user.userId);
+                cmd.Parameters.AddWithValue("@firstN", user.firstName);
+                cmd.Parameters.AddWithValue("@lastN", user.lastName);
+                cmd.Parameters.AddWithValue("@userN", user.username);
+                cmd.Parameters.AddWithValue("@pass", user.password);
+                cmd.Parameters.AddWithValue("@email", user.email);
+                try
+                {
+                    con.Open();
+                    cmd.ExecuteNonQuery();
+                    Console.WriteLine("Records Updated Successfully");
+                    logWriter = new LogWriter(user.userId, 5, DateTime.Now, "UPDATE", "UPDATE USER SUCCESSFULLY");
+                    logWriter.write();
+                }
+                catch (SqlException ex)
+                {
+                    Console.WriteLine("Error: " + ex.ToString());
+                    logWriter = new LogWriter(user.userId, 0, DateTime.Now, "SQLEXCEPTION", ex.Message);
+                    logWriter.write();
+                }
+                finally
+                {
+                    con.Close();
+                    Console.ReadKey();
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                logWriter = new LogWriter(user.userId, 1, DateTime.Now, "EXCEPTION", ex.Message);
+                logWriter.write();
+            }
+        }
+
+    }
+
+}
