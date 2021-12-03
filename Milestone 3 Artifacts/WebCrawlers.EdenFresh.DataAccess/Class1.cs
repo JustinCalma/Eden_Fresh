b9b0b0f6531e135e952e2b20424be7bf8f72da05
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-02 17:03:29 -0800

    create second library, begin LogGateway interface

---
+++ Milestone 3 Artifacts/WebCrawlers.EdenFresh.DataAccess/Class1.cs
@@ -0,0 +1,12 @@
+﻿using System.Data;
+
+namespace WebCrawlers.EdenFresh.DataAccess
+{
+    interface ILogGateway
+    {
+        public DataSet getAllLogs();
+        void deleteLogs(int userId);
+        void writeLogs(int userID);
+        void readLogs(int userId);
+    }
+}
