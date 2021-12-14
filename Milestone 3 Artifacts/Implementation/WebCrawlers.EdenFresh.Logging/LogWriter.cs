0ce51c626960ba07ee1728e81f7f70caae7560f0
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-13 17:04:49 -0800

    create logging library

---
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/LogWriter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Diagnostics;
+
+namespace WebCrawlers.EdenFresh.Logging
+{
+    class LogWriter
+    {
+        public enum LogLevel { Info, Debug, Warning, Error }
+        public enum Category { View, Business, Server, Data, DataStore }
+
+        private Stopwatch stopwatch;
+        private ILogGateway logConnection;
+      
+        public LogWriter(ILogGateway conn) 
+        {
+            this.stopwatch = new Stopwatch();
+            this.logConnection = conn;
+        }
+
+        public Boolean Write(int userId, DateTime timeStamp, LogLevel logLevel, Category category, string message)
+        {
+            stopwatch.Restart();
+            stopwatch.Start();
+            bool writeSuccessful = logConnection.Write(userId, timeStamp, logLevel,category,message);
+            stopwatch.Stop();
+            return (writeSuccessful && stopwatch.ElapsedMilliseconds <= 5000);
+        }
+    }
+}
