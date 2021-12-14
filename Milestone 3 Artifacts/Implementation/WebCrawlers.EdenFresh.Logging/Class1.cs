7a4a709a9e7fcd9edbdef617d86c8049bb8b0927
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-13 22:08:49 -0800

    create main for demonstration

---
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Data.SqlClient;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebCrawlers.EdenFresh.Logging
+{
+    class LoggingDemo
+    {
+        static void Main(string[] args)
+        {
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=logging;Integrated Security=True;Pooling=False";
+            ILogGateway gateway = new MSSQLLogGateway(connectionString);
+            LogWriter service = new LogWriter(gateway);
+        }
+    }
+}
