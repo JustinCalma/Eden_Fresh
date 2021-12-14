80803c6a0fb32b52ec61f844eb1a7bc68495bf85
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-13 18:34:26 -0800

    change method signatures to match success sequences

--- Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/ILogGateway.cs
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/ILogGateway.cs
@@ -1,10 +1,12 @@
-﻿namespace WebCrawlers.EdenFresh.Logging
+﻿using System.Data;
+namespace WebCrawlers.EdenFresh.Logging
 
 {
+    public enum Comparator {LESS, GREATER, EQUAL, NOT}
     interface ILogGateway
     {
         public Boolean WriteLog(int userId, DateTime timeStamp, LogLevel logLevel, Category category, string message);
-        public Boolean ReadLog(int logId);
-        public Boolean DeleteLog(int logId);
+        public DataTable ReadLogsWhere(string columnName, Comparator compare, string value);
+        public Boolean DeleteLogsWhere(string columnName, Comparator compare, string value);
     }
 }
