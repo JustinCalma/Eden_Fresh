a20a68f802d22857b959cce5a62278da10937469
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-13 19:18:23 -0800

    implement ReadLogsWhere()

--- Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/MSSQLLogGateway.cs
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/MSSQLLogGateway.cs
@@ -23,9 +23,42 @@ namespace WebCrawlers.EdenFresh.Logging
 
         public DataTable ReadLogsWhere(string columnName, Comparator compare, string value)
         {
-            using(SqlConnection conn = new SqlConnection(this.connectionString))
+            SqlConnection conn = null;
+            try
             {
-
+                conn = new SqlConnection(connectionString);
+                string query = "Select * from Logger where ";
+                switch (compare)
+                {
+                    case Comparator.LESS:
+                        query += "< @value";
+                        break;
+                    case Comparator.GREATER:
+                        query += "> @value";
+                        break;
+                    case Comparator.EQUAL:
+                        query += "= @value";
+                        break;
+                    case Comparator.NOT:
+                        query += "<> @value";
+                        break;
+                }
+                conn.Open();
+                SqlCommand command = new SqlCommand(query, conn);
+                command.Parameters.AddWithValue("@value", value);
+                SqlDataReader reader = command.ExecuteReader();
+                DataTable table = new DataTable();
+                table.Load(reader);
+                return table;
+            }
+            catch(SqlException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return new DataTable ();
+            }
+            finally
+            {
+                if (conn != null) conn.Close();
             }
         }
 
