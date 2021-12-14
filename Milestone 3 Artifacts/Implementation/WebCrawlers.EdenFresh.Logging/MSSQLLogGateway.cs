6b8e63a7bb2ccc861bd79d9a1faac33a7ca75d08
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-13 18:34:40 -0800

    follow interface changes

--- Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/MSSQLLogGateway.cs
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.Logging/MSSQLLogGateway.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,14 +10,23 @@ namespace WebCrawlers.EdenFresh.Logging
 {
     class MSSQLLogGateway : ILogGateway
     {
-        public bool DeleteLog(int logId)
+        private string connectionString;
+
+        public MSSQLLogGateway(string connection)
+        {
+            this.connectionString = connection;
+        }
+        public bool DeleteLogsWhere(string columnName, Comparator compare, string value)
         {
             throw new NotImplementedException();
         }
 
-        public bool ReadLog(int logId)
+        public DataTable ReadLogsWhere(string columnName, Comparator compare, string value)
         {
-            throw new NotImplementedException();
+            using(SqlConnection conn = new SqlConnection(this.connectionString))
+            {
+
+            }
         }
 
         public bool WriteLog(int userId, DateTime timeStamp, LogLevel logLevel, Category category, string message)
