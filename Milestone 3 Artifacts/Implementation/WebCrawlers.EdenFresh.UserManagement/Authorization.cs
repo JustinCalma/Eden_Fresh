3ff21534a7239cb4900421cde9d11a9fbd8bfd35
SzeManTang <72945488+SzeManTang@users.noreply.github.com>
2021-12-14 20:02:09 -0800

    Update Authorization.cs

--- Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.UserManagement/Authorization.cs
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.UserManagement/Authorization.cs
@@ -6,7 +6,23 @@ using System.Threading.Tasks;
 
 namespace WebCrawlers.EdenFresh.UserManagement
 {
-    internal class Authorization
+    internal class Authorization : Authentication
     {
+        private Authentication umAuthen;
+        private int userId;
+        private Random rnd = new Random();
+
+        private Stopwatch stopwatch;
+
+        public umAuthorization(Authentication umAuthen)
+        {
+            this.umAuthentication = umAuthen;
+            userId = rnd.Next(100000, 999999);
+        }
+
+        public Boolean AuthorizeUser(string userId)
+        {
+            return(umAuthentication.AuthenthicationUser(userId));
+        }
     }
 }
