3a51627d799efbdb863cacc02d519dbd7db44147
alexyung <48598834+alexyung@users.noreply.github.com>
2021-12-15 00:17:01 -0800

    dependency injection; test all single UM operations

--- Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.UserManagement/UMEntry.cs
+++ Milestone 3 Artifacts/Implementation/WebCrawlers.EdenFresh.UserManagement/UMEntry.cs
@@ -1,21 +1,48 @@
-﻿namespace WebCrawlers.EdenFresh.UserManagement
+﻿using WebCrawlers.EdenFresh.Logging;
+
+namespace WebCrawlers.EdenFresh.UserManagement
 {
     class UMEntry
     {
         //Demo Class
         
-          static void Main(String[] args)
-          {
-                string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=l
-ogging;Integrated Security=True;Pooling=False";
-                MSSQLUMGateway gateway = new MSSQLUMGateway(connectionString);
-            //gateway.WriteToDataStore("email", "password", true);
-            //gateway.DeleteFromDataStore(729527, "gmail", "password", true);
-            gateway.UpdateDataStore(593436, "updatePassword", "UpdateEmail", false);
+        static void Main(String[] args)
+        {
+            Random rnd = new Random();
+            string connectionString = @"Data Source=.\SQLEXPRESS;Initial Catalog=logging;Integrated Security=True;Pooling=False";
+            MSSQLUMGateway userGateway = new MSSQLUMGateway(connectionString);
+            MSSQLLogGateway logGateway = new MSSQLLogGateway(connectionString);
+            Authentication authN = new Authentication();
+            Authorization authZ = new Authorization(authN);
+            // chose an ID that exists in the testing database
+            int inititatingUser = 430435;
+            UMService serv = new UMService(userGateway);
+            LogWriter logger = new LogWriter(logGateway);
 
-           }
-        
+            UMManager manager = new UMManager(inititatingUser, serv, authZ, logger);
 
+            // create random accounts
+            for(int i = 0; i < 5; i++)
+            {
+                manager.CreateAccount($"user{rnd.Next()}@email.com", $"password{rnd.Next()}", true);
+            }
 
+            // delete an account
+            manager.DeleteAccount(593436, "email", "password");
+
+            // update an account
+            manager.UpdateAccount(506254, "email", "differentpassword", false);
+
+            // disable some accounts
+            manager.DisableAccount(506254, "email", "differentpassword");
+
+            manager.DisableAccount(267842, "email", "password");
+
+            Console.WriteLine("Please observe the disabled accounts.");
+            Console.ReadKey();
+
+            // enable a disabled account
+            manager.EnableAccount(267842, "email", "password");
+        }
     }
 }
